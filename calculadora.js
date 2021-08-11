9330f1ad38bb7d831d3b95b55055257d907ff30b
LuchoMorla <luis-fernando-10@outlook.es>
2021-08-10 23:53:07 -0500

    realice la conversion de unidades, agregue un checkbox para validar con el cliente si gustaria pagar por el diseño, actualice la pagina en ingles, etc.

--- calculadora.js
+++ calculadora.js
@@ -1,24 +1,55 @@
+// Se agregara el valor adicional por el diseño
+
+function sumar(respuesta){
+    sum = document.getElementById('suma');
+    if(respuesta > 0){
+     sum.innerHTML = "Aditional value for the desing: " + respuesta + "dollars";
+    } else if(respuesta = 0) {
+     sum.innerHTML = "Aditional value for the desing: " + 0 + "dollars";
+    }
+ }
+ 
+
+// Validaremos el checkbox en el que le preguntamos al cliente si quiere incluir el diseño de su producto
+
+function validar() {
+    var checkbox = document.getElementById('wantDiseño');
+    checkbox.addEventListener('change', validar, false);
+    var checked = checkbox.checked;
+    if(checked){
+        respuesta = 35;
+    }else{
+        respuesta = 0;
+    }
+    sumar(respuesta);
+}
+
 // Interaccion con html dentro de la funcion que va a sacar el resultado del calculo
+
+let pulgadasSobreCentimetro = 39e-2;
 
 function calculoX(){
     let l = document.getElementById("longitud");
     let w = document.getElementById("ancho");
-    let length = l.value;
-    let width = w.value;
+    var length = l.value;
+    var width = w.value;
 
-    let x = length * width;
-    let calcularAdhesive = calcAdhesiV(x);
+    var haveDesing = validar();
 
-    console.log(calcularAdhesive);
+    var lengthPulgadas = length * pulgadasSobreCentimetro;
+    var widthPulgadas = width * pulgadasSobreCentimetro;
+
+    let x = lengthPulgadas * widthPulgadas;
+    let calcularAdhesive = calcAdhesiV(x, haveDesing);
 
     y = document.getElementById("area");
-    y.innerHTML = "El resultado de Y o del area es: " + calcularAdhesive;
+    y.innerHTML = "The 'value:¿Cual era el resultado?' of your Adeshive Vinyl is: " + calcularAdhesive; + " Inches"
 }
 
 // calculamos el valor de y, o el area, utilizando la funcion Math.sqrt para calcular la raiz
 
 function calcAdhesiV(x){
     let parentesis = (Math.sqrt(x * 2) * 21)
-    return parentesis + 50;
-    console.log(parentesis);
+    return parentesis
 }
+
